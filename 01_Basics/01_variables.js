6aaa2d52f6438c494c2f4a22d85c53c1209da0ac
Shivam Yadav <shivamptu4566@gmail.com>
2024-10-03 11:03:55 +0000

    dataTypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail="shivam@gmail.com"
+var accountPassword="12345"
+ accountCity = "Jaipur"
+let accountState;
+
+
+// accountId = 2 //Not allowed
+
+accountEmail="shiva01@gmail.com"
+accountPassword = "212121"
+accountCity = "varanasi"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword, accountCity,accountState])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict";// treat all js code as newer version
+
+// alert( 3+3 )//we are using nodejs,not browser
+
+
+// console.log(3+3); //code readability is very high
+console.log("Shivam");
+
+let name ="shivam"
+let age = 18
+let isLoggedIn = false
+
+b 
