7eecd8ef0a666ed2111e06f2b25d9bd165f9c10c
Shivam Yadav <shivamptu4566@gmail.com>
2024-10-05 09:55:32 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+let isLoggedIn =1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber  = String(someNumber)
+console.log(stringNumber);
+console.log(type of stringNumber);
